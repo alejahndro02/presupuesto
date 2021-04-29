c2535689bb014b8f4562048facf436570fcfc12b
Alejahndro <alejahndro02@gmail.com>
2021-04-29 18:09:15 -0500

    Se elimino el componente ingresos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
+import { PresupuestoComponent } from './components/presupuesto/presupuesto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent,
+    PresupuestoComponent
   ],
   imports: [
     BrowserModule,
