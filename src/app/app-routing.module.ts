6bad6626353577605a52a328d3a454e812ea4740
Alejahndro <alejahndro02@gmail.com>
2021-05-01 11:12:21 -0500

    Se agregaron las rutas para que se rendericen los componentes correpondientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//componentes
+import { PresupuestoComponent } from './components/presupuesto/presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo : '/presupuesto', pathMatch: 'full' },
+  { path:'presupuesto', component : PresupuestoComponent},
+  { path:'gastos', component : GastosComponent},
+  { path:'**', redirectTo :'/presupuesto', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
