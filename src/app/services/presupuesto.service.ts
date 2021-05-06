a90b090f581da3d2838c17c813dbbab16600a677
Alejahndro <alejahndro02@gmail.com>
2021-05-06 15:04:08 -0500

    Se complemento el metodo agregarGasto y se creo el metodo getGasto tambien se creo el obserbable gasto$

--- src/app/services/presupuesto.service.ts
+++ src/app/services/presupuesto.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,11 +7,17 @@ import { Injectable } from '@angular/core';
 export class PresupuestoService {
   presupuesto:  number;
   restante : number;
+  private gastos$ = new Subject<any>();
   constructor() { 
     this.presupuesto = 0;
     this.restante = 0;
   }
   agregarGasto(gasto:any){
     this.restante = this.restante - gasto.cantidad;
+    this.gastos$.next(gasto)
   }
+  getGasto():Observable<any>{
+    return this.gastos$.asObservable()
+  }
+
 }
