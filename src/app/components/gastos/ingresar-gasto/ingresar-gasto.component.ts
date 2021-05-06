1d9240372d67a925c851da2ce88422e2ffeba2a4
Alejahndro <alejahndro02@gmail.com>
2021-05-06 12:54:45 -0500

    Se importo el servicio atraves de contructor y se hizo una validación si el presupuesto es manor a la cantidad

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,3 +1,4 @@
+import { PresupuestoService } from './../../../services/presupuesto.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,21 +10,36 @@ export class IngresarGastoComponent implements OnInit {
   nombreGasto: string;
   cantidad:number
   formularioIncorrecto:boolean;
-  cantidadMayorPresupuesto:string;
+  textoError:string;
 
-  constructor() {
+  constructor(private _presupuestoService: PresupuestoService) {
     this.nombreGasto= '';
     this.cantidad=0;
     this.formularioIncorrecto= false;
-    this.cantidadMayorPresupuesto='Nombre Gasto o Cantidad Incorrecta';
+    this.textoError='Nombre Gasto o Cantidad Incorrecta';
   }
 
   ngOnInit(): void {
   }
   agergarGasto(){
+    if(this.cantidad > this._presupuestoService.restante){
+    this.formularioIncorrecto= true;
+    this.textoError='La cantidad es mayor al presupuesto';
+    return;
+    }
     if(this.nombreGasto === '' || this.cantidad <= 0){
       this.formularioIncorrecto = true;
     } else {
+      //Se declara un objeto
+      const GASTO={
+        nombre:this.nombreGasto,
+        cantidad:this.cantidad
+      }
+
+      //se envia el objeto atraves del subject
+      this._presupuestoService.agregarGasto(GASTO)
+
+      //Reset form 
       this.formularioIncorrecto = false;
       this.nombreGasto= '';
       this.cantidad=0;
