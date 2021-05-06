5d88bb35c1e3995699f1d6f7e2bd1f003060c196
Alejahndro <alejahndro02@gmail.com>
2021-05-06 17:31:04 -0500

    se declararon las variables subscription, presupuesto y restante, por medio del constructor se mando a llamar al presupuestoService

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,19 +1,32 @@
 import { PresupuestoService } from './../../../services/presupuesto.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent implements OnInit {
+export class ListarGastoComponent implements OnInit, OnDestroy {
+  subscription : Subscription;
+  presupuesto: number;
+  restante: number;
 
   constructor(private _presupuestoService: PresupuestoService) {
-    this._presupuestoService.getGasto().subscribe(data => console.log(data)
+    this.presupuesto = 0 ;
+    this.restante = 0;
+    this.subscription = this._presupuestoService.getGasto().subscribe(data => console.log(data)
     );
   }
 
   ngOnInit(): void {
-  }
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
 
+  }
+  ngOnDestroy():void{
+    {
+      this.subscription.unsubscribe();
+    }
+  }
 }
